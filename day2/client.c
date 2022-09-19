b0ebda3fba53794ba48f5e6560e5858ef98b64da
prathvikshervegar <shervegarprathvik@gmail.com>
2022-09-19 12:44:42 +0530

    day1 old commit

--- day2/client.c
+++ day2/client.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "list.h"
 
 int main(){
     FILE* fp;
     fp = fopen("customer.dat", "r"); 
-
-    read_cust_data(fp);
+    struct person p[MAX_CUST];
+    read_cust_data(fp, p);
 
     struct date start_date;
     printf("Enter billing start date(dd mm yyyy): ");
@@ -15,7 +16,15 @@ int main(){
     printf("Enter billing end date(dd mm yyyy): ");
     scanf("%d %d %d", &end_date.dd, &end_date.mm, &end_date.yy);
 
-    generate_bill(&start_date, &end_date);
+    if(! is_valid_dates(&start_date, &end_date)){
+        printf("Invalid date provided!!!");
+        exit(1);
+    }
 
-    fclose(fp);
+    int n = date_diff(&start_date, &end_date);
+    struct bill b[n+1];
+
+    generate_bill(b, &start_date, n+1, p);
+    disp_data_by_day(b, n+1, p);
+    disp_data_by_person(b, n+1, p);
 }
